cd674ecbbdb7f8350a69d553e828a5065832ba08
Jiří Matějka <jura.matejka@gmail.com>
2019-07-12 16:48:27 +0200

    Initial commit with skeleton of method GetAllResumePoints

---
+++ VideoApi/Controllers/AccountVideoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VideoApi.Controllers
+{
+    [Route("api/accounts/")]
+    public class AccountsVideoController : Controller
+    {
+        [HttpGet("{accountId}/resumepoints")]
+        public Task<object> GetAllResumePoints(int accountId)
+        {
+            var firstElem = new
+            {
+                account = 123,
+                resumePoint = 13.3456
+            };
+
+            var result = new[]
+            {
+                firstElem
+            };
+
+            return Task.FromResult((object)result);
+        }
+    }
+}
