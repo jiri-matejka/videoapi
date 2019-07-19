b51074c7e44dc6923615d4cb65993f904d463d07
Jiří Matějka <jura.matejka@gmail.com>
2019-07-19 17:11:35 +0200

    refactor: Move account collection creation to the constructor

--- VideoApi.Data.MongoDb/ResumePointRepository.cs
+++ VideoApi.Data.MongoDb/ResumePointRepository.cs
@@ -12,33 +12,31 @@ namespace VideoApi.Data.MongoDb
 	public class ResumePointRepository : IResumePointRepository
 	{
 		private readonly IMongoDatabase database;
+		private readonly IMongoCollection<Account> accountCollection;
 
 		public ResumePointRepository(IMongoDatabase database)
 		{
 			this.database = database;
+			this.accountCollection = this.database.GetCollection<Account>("accounts");
 		}
 
 		public async Task<ResumePoint> Get(string accountId, string videoId)
 		{
-			IMongoCollection<Account> collection = this.database.GetCollection<Account>("accounts");
-
 			FilterDefinition<Account> filter = 
 				Builders<Account>.Filter.Eq("_id", new ObjectId(accountId)) &
 				Builders<Account>.Filter.ElemMatch(a => a.ResumePoints, rp => rp.VideoId == new BsonObjectId(new ObjectId(videoId)));
 
-			var result = await collection.Find(filter).SingleOrDefaultAsync();
+			var result = await this.accountCollection.Find(filter).SingleOrDefaultAsync();
 
 			return result.ResumePoints.SingleOrDefault();
 		}
 
 		public async Task<IReadOnlyList<ResumePoint>> GetAll(string accountId)
 		{
-			IMongoCollection<Account> collection = this.database.GetCollection<Account>("accounts");
-
 			FilterDefinition<Account> accountFilter = new BsonDocument(
 				new BsonElement("_id", new BsonObjectId(new ObjectId(accountId))));
 
-			Account account = await collection.Find(accountFilter).FirstOrDefaultAsync();
+			Account account = await this.accountCollection.Find(accountFilter).FirstOrDefaultAsync();
 			if (account == null)
 				throw new ArgumentException($"Account {accountId} does not exist");
 
